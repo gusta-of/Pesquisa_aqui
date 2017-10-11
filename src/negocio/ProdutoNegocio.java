c72ea0083c80284b2670f10e1fffa9c975fe29e5
Gustavo <gustavofernandes97.12@gmail.com>
2017-10-11 08:31:34 -0300

    Criação da classe Produto da camada de negocio

---
+++ src/negocio/ProdutoNegocio.java
@@ -0,0 +1,17 @@
+package negocio;
+
+public class ProdutoNegocio {
+	
+	public String salvar() {
+		return null;
+	}
+	
+	public boolean validarIdade() {
+		return true;
+	}
+	
+	public String validarCampos() {
+		return null;
+	}
+
+}
