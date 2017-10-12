5986631fef8d3a907d04d38f66f4943815265fc0
Certs <Certs@192.168.1.18>
2017-10-12 12:19:06 -0300

    Adicionado regra de negocio (ValidarCampos) ProdutoNegocio

--- src/negocio/ProdutoNegocio.java
+++ src/negocio/ProdutoNegocio.java
@@ -1,5 +1,7 @@
 package negocio;
 
+import model.Produto;
+
 public class ProdutoNegocio {
 	
 	public String salvar() {
@@ -10,8 +12,22 @@ public class ProdutoNegocio {
 		return true;
 	}
 	
-	public String validarCampos() {
-		return null;
+	public String validarCampos(Produto produto) {
+		StringBuilder inconsistencias = new StringBuilder();
+		if(produto.getCodigo() <= 0) {
+			inconsistencias.append("\nO produto não pode ser cadastrado com um codigo menor ou igual a 0.");
+		}
+		if(produto.getNomeProduto().equals("") || produto.getNomeProduto() == null) {
+			inconsistencias.append("\nO nome do produto é obrigatório.");
+		}
+		if(produto.getDescricao().equals("") || produto.getDescricao() == null) {
+			inconsistencias.append("\nA descrição do produto é obrigatório.");
+		}
+		if(produto.getValor() <= 0) {
+			inconsistencias.append("\nO produto não pode ser cadastrado com valor menor ou igual a 0.");
+		}
+		System.out.println(inconsistencias.toString());
+		return  inconsistencias.toString();
 	}
 
 }
