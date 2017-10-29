f13f5834a8111223e8ec1549d18e8daec65d2f57
Gustavo <gustavofernandes97.12@gmail.com>
2017-10-28 23:56:52 -0200

    1. Tela de Login funcionando

--- src/controller/LoginController.java
+++ src/controller/LoginController.java
@@ -1,32 +1,55 @@
 package controller;
 
+import java.io.IOException;
 import java.net.URL;
-import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 
-public class LoginController implements Initializable{
-	
+public class LoginController {
+
 	@FXML
 	private AnchorPane apnPaneLogin, apnBarra;
-	
-	@FXML	
+
+	@FXML
 	private TextField txtUser, txtSenha;
-	
-	@FXML 
+
+	@FXML
 	private Button btLoguin;
-	
-	@FXML 
+
+	@FXML
 	private ImageView imgLogo, imgBusto, imgCadeado, imgPc, imgLoad2;
+	
+	@FXML
+	private Label lblDados;
 
-	@Override
-	public void initialize(URL location, ResourceBundle resources) {
+	public void logar() throws IOException {
 		
+		String login;
+		String senha;
+		login = txtUser.getText();
+		senha = txtSenha.getText();
+		if (login.equals("usuario") && senha.equals("123456")) {
+			URL arquivoFxml;
+			arquivoFxml = getClass().getResource("/visao/Cadastro de fornecedor.fxml");
+			Parent fxmlParent = (Parent) FXMLLoader.load(arquivoFxml); 
+			apnPaneLogin.getChildren().clear();
+			apnPaneLogin.getChildren().add(fxmlParent);
+			imgLoad2.setVisible(true);
+		} else {
+			lblDados.setText("Dados incoretos!");
+			lblDados.setVisible(true);
+		}
 	}
-
+	
+	public void limpar() {
+		txtUser.setText("");
+		txtSenha.setText("");
+	}
 }
