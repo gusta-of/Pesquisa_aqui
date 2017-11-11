183e1a067903a1316fc36316e70dc2dc5d855a04
Gustavo <gustavofernandes97.12@gmail.com>
2017-11-11 14:08:17 -0200

    1. Implementações em login

--- src/dao/LoginDao.java
+++ src/dao/LoginDao.java
@@ -1,29 +1,48 @@
 package dao;
 
 import java.sql.Connection;
-import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 import daoUtil.ConnectionFactory;
 import model.Admin;
 
 public class LoginDao {
-   
-   private PreparedStatement stmt;
-   private Connection con;
-   private Statement stm;
-   
-   Connection connection = null;
-   
-   public LoginDao() {
-      ConnectionFactory cf = new ConnectionFactory();
-      con = cf.getConnection();
-   }
-   
-   String sqlLogar = "SELECT * FROM admin WHERE usuario = usurario AND senha = senha";
-   
-   public String login(Admin admin) {
-      String login = "falha";
-      return null;
-   }
+
+	private Connection con;
+	private Statement stm;
+
+	Connection connection = null;
+
+	public LoginDao() {
+		ConnectionFactory cf = new ConnectionFactory();
+		con = cf.getConnection();
+	}
+
+	String sqlLogar = "SELECT * FROM admin WHERE usuario = usurario AND senha = senha";
+
+	public List<Admin> listarAdmin() {
+		List<Admin> lista = new ArrayList<Admin>();
+		ResultSet res = null;
+		try {
+			if (this.con != null) {
+				stm = con.createStatement();
+				res = stm.executeQuery(sqlLogar);
+				while (res.next()) {
+					Admin login = new Admin();
+					login.setUsuario(res.getString("usuario"));
+					login.setSenha(res.getString("senha"));
+					
+					lista.add(login);
+				}
+			}
+			return lista;
+		} catch (SQLException e) {
+			System.out.println("Erro na consulta SQL!" + e.getMessage());
+			return new ArrayList<Admin>();
+		}
+	}
 }
---
+++ src/negocio/LoginNegocio.java
@@ -0,0 +1,15 @@
+package negocio;
+
+import dao.LoginDao;
+
+public class LoginNegocio {
+	
+	LoginDao loginDao = new LoginDao();
+	
+	public boolean verificaUsuarioEsenha() {
+		
+		if(loginDao.listarAdmin() == )
+		return true;
+	}
+	
+}
